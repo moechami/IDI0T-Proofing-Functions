bd6a3fdf6278f8d32a397c292272e7ee34bc1da5
Mo Chami <moeelchami@gmail.com>
2024-03-10 17:18:33 -0400

    1st save

--- Part1.cpp
+++ Part1.cpp
@@ -2,12 +2,45 @@
 
 using namespace std;
 
-int getIntValues () {
+int getIntValue (const string& message = "Please Enter an Integer: ") {
 
-    int values;
+    int inputValue;
 
-    cin >> values;
+    while (true) {
 
-    return values;
+        cout << message << endl;
+        cin >> inputValue;
 
+        if (!cin.fail()) {
+
+            break;
+        } else {
+            cin.clear();
+            cin.ignore(10000);
+        }
+    }
+ 
+    return inputValue;
+
+}
+
+bool getIntValue (int& enteredValue, const string& message = "Please Enter an Integer: ", int n = 3) {
+
+    int attempts = 0;
+
+    while (attempts <= n) {
+        cout << message << endl;
+        cin >> enteredValue;
+
+        if (!cin.fail()) {
+            
+            return true;
+        } else {
+            cin.clear();
+            cin.ignore(10000);
+            attempts++;
+        }
+    }
+    
+    return false;
 }
--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Hello Everyone!
 
 This is a public repo designed to help and assist anyone that it may come by. Feel free to copy this code or be inspired by it :)
 
-These functions are meant to get input from the user, validate it, and return it; nothing more. This will include 36 user-input validation functions that you can use for years to come when writing other programs.
+These functions are meant to get input from the user, validate it, and return it; nothing more. Therefore, none of these functions may display any message or values other than the original message and a request to try again. Remember, cin knows what data type it’s trying to read and will return an error if the data type is wrong, so there’s no need to read data as a string and parse it yourself.
+
+This will include 36 user-input validation functions that you can use for years to come when writing other programs. 
